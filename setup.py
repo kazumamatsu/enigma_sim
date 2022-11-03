efe747cc3ded1f8eccab846ae113e56635e114c5
kazumamatsu <64215585+kazumamatsu@users.noreply.github.com>
2022-11-03 18:42:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ import setuptools
 setuptools.setup(
     name="enigma",
     version="1.0",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     entry_points = {
         'console_scripts': ['enigma = enigma.Enigma_simulater:main']
     }
