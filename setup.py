1ff82cb2ae8d6a51dadbb5eecf6122eb8ee71009
kazumamatsu <64215585+kazumamatsu@users.noreply.github.com>
2022-11-03 19:03:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="enigma",
     version="1.0",
+    author="kazumamatsu",
     entry_points = {
         'console_scripts': ['enigma = enigma.enigma_simulater:enigma']
     }
