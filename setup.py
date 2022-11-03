30758b077812f2279b6a2afcb912524b0dbe3abd
kazumamatsu <64215585+kazumamatsu@users.noreply.github.com>
2022-11-03 19:30:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ import setuptools
 setuptools.setup(
     name="enigma",
     version="1.0",
+    description="3rotors eniguma machine simulater",
     author="kazumamatsu",
+    url = "https://github.com/kazumamatsu/enigma_sim.git",
     entry_points = {
         'console_scripts': ['enigma = enigma.enigma_simulater:enigma']
     }
