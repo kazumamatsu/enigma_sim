2afef95911233d1187650cdee89183473fddff7e
kazumamatsu <64215585+kazumamatsu@users.noreply.github.com>
2022-11-03 22:18:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     author="kazumamatsu",
     url = "https://github.com/kazumamatsu/enigma_sim.git",
     packages=setuptools.find_packages("enigma_simulater"),
+    requires=["string","random","sys"],
     entry_points = {
         'console_scripts': ['enigma = enigma.enigma_simulater:enigma']
     }
